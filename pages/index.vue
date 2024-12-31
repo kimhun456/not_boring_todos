b33adf32dc70e31d599aadfe051e98769e1c14f4
HyunJae KIM <kimhun456@gmail.com>
2024-12-31 06:27:22 +0000

    Add shadcn/vue for compoment ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,17 @@
+<script setup lang="ts">
+let message = ref("message");
+
+function onClick() {
+  console.log("onClick()");
+  message.value = "message is changed";
+}
+</script>
+
 <template>
   <div>
     <h1>Welcome to the homepage</h1>
-    <AppAlert>
-      This is an auto-imported component
-    </AppAlert>
+    <AppAlert> This is an auto-imported component </AppAlert>
+    <Button @click="onClick">{{ message }}</Button>
+    <Button>Button2</Button>
   </div>
 </template>
